2e6abd8f80c2b84bca2824b5467d1f40007a23bd
Claymont <claymont@mail.com>
2019-03-22 14:32:45 +0100

    Add files via upload

---
+++ background.js
@@ -0,0 +1,28 @@
+﻿browser.commands.onCommand.addListener(function(command) {
+
+  function getCurrentWindowTabs() {
+    return browser.tabs.query({currentWindow: true});
+  }
+
+  function callOnActiveTab(callback) {
+    getCurrentWindowTabs().then((tabs) => {
+      for (var tab of tabs) {
+        if (tab.active) {
+          callback(tab, tabs);
+        }
+      }
+    });
+  }
+
+  if (command == "detach-tab") {
+    callOnActiveTab((tab, tabs) => {
+      var creating = browser.windows.create({
+        tabId: tab.id
+      });
+
+    });
+  }
+
+  console.log("Tab Detached!");
+
+});
